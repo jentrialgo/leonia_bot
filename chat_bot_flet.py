4e1fdf1adb59dd3988579b3cf2c93476c8cb4ea0
Joaquín Entrialgo <jentrialgo@gmail.com>
2023-04-27 23:18:05 +0200

    Make the configuration work
    A new class was added to mantain the state of the chat window

--- chat_bot_flet.py
+++ chat_bot_flet.py
@@ -8,7 +8,7 @@ import flet as ft
 
 from chat_bot import ChatBot, BOT_NAME, HH_MODEL_REPOS
 from conversation import Conversation
-from configuration import ConfigurationControl, init_config
+from configuration import ConfigurationControl, get_init_config
 
 INITIAL_MSG = "Hello, how can I help you?"
 
@@ -40,83 +40,35 @@ class TextWithWrite(ft.Text):
         self.value = ("").join(self.lines)
         self.update()
 
+    def reset(self) -> None:
+        """Reset the log text control."""
+        self.value = ""
+        self.lines = []
+        self.update()
 
-def main(page: ft.Page):
-    """This function is called when the application starts. It creates the
-    flet page and adds the controls to it."""
 
-    def on_submit(event: ft.ControlEvent) -> None:
-        """This function is called when the user clicks the Submit button. It
-        sends the message to the chat bot and displays the answer in the
-        conversation."""
-        human_msg = tf_input.value
+class ChatBotApp:
+    def __init__(self, page: ft.Page):
+        self.page = page
 
-        # If the user clicks the Submit button without entering a message,
-        # do nothing.
-        if human_msg == "":
-            return
+        page.scroll = True
+        page.theme = ft.Theme(color_scheme_seed="#008b84")
+        page.title = f"{BOT_NAME} chat"
+        page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+        page.vertical_alignment = ft.CrossAxisAlignment.CENTER
 
-        tf_input.value = ""
-        tf_input.update()
+        img = ft.Image(
+            src="/lion.png",
+            width=200,
+            height=200,
+            fit=ft.ImageFit.CONTAIN,
+        )
 
-        conversation.add_user_msg(human_msg)
+        configuration_control = ConfigurationControl(page)
+        col_content = ft.Column(controls=[img], auto_scroll=True)
+        col_config = ft.Column(controls=[configuration_control])
 
-        progress.visible = True
-        progress.update()
-
-        start = time.time()
-        answer_iterator = chat_bot.get_answer(human_msg)
-        answer = next(answer_iterator)
-        end = time.time()
-        elapsed_sec = end - start
-
-        conversation.add_bot_msg(answer, elapsed_sec)
-
-        for answer in answer_iterator:
-            end = time.time()
-            elapsed_sec = end - start
-            try:
-                conversation.append_bot_msg(answer, elapsed_sec)
-            except:
-                # This can happen if the user clicks "Clear" while the AI is
-                # thinking
-                return
-
-        progress.visible = False
-        progress.update()
-
-        tf_input.update()
-        tf_input.focus()
-
-    def on_clear(event: ft.ControlEvent) -> None:
-        """This function is called when the user clicks the Clear button. It
-        clears the conversation."""
-        conversation.clear()
-        conversation.add_bot_msg(INITIAL_MSG)
-        progress.visible = False
-        progress.update()
-
-    page.scroll = True
-    page.theme = ft.Theme(color_scheme_seed="#008b84")
-    page.title = f"{BOT_NAME} chat"
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.vertical_alignment = ft.CrossAxisAlignment.CENTER
-
-    img = ft.Image(
-        src="/lion.png",
-        width=200,
-        height=200,
-        fit=ft.ImageFit.CONTAIN,
-    )
-
-    page.add()
-
-    configuration_control = ConfigurationControl(page)
-    col_content = ft.Column(controls=[img], auto_scroll=True)
-    col_config = ft.Column(controls=[configuration_control])
-
-    page.add(
-        ft.Tabs(
+        self.tabs = ft.Tabs(
             expand=1,
             selected_index=0,
             animation_duration=300,
@@ -130,71 +82,160 @@ def main(page: ft.Page):
                 ),
                 ft.Tab(text="Configuration", content=col_config),
             ],
-        ),
-    )
+        )
 
-    conversation = Conversation()
-    col_content.controls.append(conversation.col_conversation)
+        page.add(self.tabs)
 
-    progress_text = ft.Text("I'm turning on. Please wait...")
-    progress = ft.Column(
-        [ft.ProgressRing(), progress_text],
-        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-    )
-    col_content.controls.append(progress)
+        self.conversation = Conversation()
+        col_content.controls.append(self.conversation.col_conversation)
 
-    # Create a text control for the log
-    text_log = TextWithWrite()
-    col_content.controls.append(text_log)
-    page.update()
+        self.progress_text = ft.Text("I'm turning on. Please wait...")
+        self.progress = ft.Column(
+            [ft.ProgressRing(), self.progress_text],
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        )
+        col_content.controls.append(self.progress)
 
-    # Other alternative models (require changes in the tokenizer and the
-    # Transformer classes in the chat_bot module):
-    # - EleutherAI/pythia-70m
-    # - bigscience/bloom-7b1
-    # - theblackcat102/pythia-3b-deduped-sft-r1
-    # - Dahoas/pythia-6B-sft-response-full-static
-    # - OpenAssistant/oasst-sft-1-pythia-12b
-    # - oasst-sft-4-pythia-12b-epoch-3.5
-    # - distilgpt2
-    # - cerebras/Cerebras-GPT-2.7B
-    # - databricks/dolly-v2-12b
-    # - StableLM-Tuned-Alpha-7b Chat
-    model_name = init_config(page)
-    with redirect_stdout(text_log), redirect_stderr(text_log):
-        chat_bot = ChatBot(model_name)
-        chat_bot.initialize()
+        # Create a text control for the log
+        self.text_log = TextWithWrite()
+        col_content.controls.append(self.text_log)
+        page.update()
 
-    text_log.value = ""
-    text_log.visible = False
-    text_log.update()
+        # Other alternative models (require changes in the tokenizer and the
+        # Transformer classes in the chat_bot module):
+        # - EleutherAI/pythia-70m
+        # - bigscience/bloom-7b1
+        # - theblackcat102/pythia-3b-deduped-sft-r1
+        # - Dahoas/pythia-6B-sft-response-full-static
+        # - OpenAssistant/oasst-sft-1-pythia-12b
+        # - oasst-sft-4-pythia-12b-epoch-3.5
+        # - distilgpt2
+        # - cerebras/Cerebras-GPT-2.7B
+        # - databricks/dolly-v2-12b
+        # - StableLM-Tuned-Alpha-7b Chat
+        self.model_name = get_init_config(page)
+        self.init_model()
 
-    progress.visible = False
-    progress.controls[1].value = "Thinking..."
+        configuration_control.set_conf_change_hook(self.change_conf)
 
-    conversation.add_bot_msg(INITIAL_MSG)
+        self.progress.visible = False
+        self.progress.controls[1].value = "Thinking..."
 
-    button_clear = ft.FilledButton(
-        text="Clear",
-        icon=ft.icons.CLEAR,
-        on_click=on_clear,
-    )
-    tf_input = ft.TextField(
-        hint_text="Type your message here",
-        on_submit=on_submit,
-        expand=True,
-        multiline=True,
-    )
-    button_submit = ft.FilledButton(
-        text="Submit",
-        on_click=on_submit,
-    )
+        self.conversation.add_bot_msg(INITIAL_MSG)
 
-    row_input = ft.Row(
-        [tf_input, button_submit, button_clear],
-    )
-    col_content.controls.append(row_input)
-    page.update()
+        button_clear = ft.FilledButton(
+            text="Clear",
+            icon=ft.icons.CLEAR,
+            on_click=self.on_clear,
+        )
+        self.tf_input = ft.TextField(
+            hint_text="Type your message here",
+            on_submit=self.on_submit,
+            expand=True,
+            multiline=True,
+        )
+        button_submit = ft.FilledButton(
+            text="Submit",
+            on_click=self.on_submit,
+        )
+
+        row_input = ft.Row(
+            [self.tf_input, button_submit, button_clear],
+        )
+        col_content.controls.append(row_input)
+        page.update()
+
+    def init_model(self):
+        with redirect_stdout(self.text_log), redirect_stderr(self.text_log):
+            self.chat_bot = ChatBot(self.model_name)
+            self.chat_bot.initialize()
+
+        self.text_log.reset()
+        self.text_log.visible = False
+        self.text_log.update()
+
+    def on_submit(self, event: ft.ControlEvent) -> None:
+        """This function is called when the user clicks the Submit button. It
+        sends the message to the chat bot and displays the answer in the
+        conversation."""
+        human_msg = self.tf_input.value
+
+        # If the user clicks the Submit button without entering a message,
+        # do nothing.
+        if human_msg == "":
+            return
+
+        self.tf_input.value = ""
+        self.tf_input.update()
+
+        self.conversation.add_user_msg(human_msg)
+
+        self.progress.visible = True
+        self.progress.update()
+
+        start = time.time()
+        answer_iterator = self.chat_bot.get_answer(human_msg)
+        answer = next(answer_iterator)
+        end = time.time()
+        elapsed_sec = end - start
+
+        self.conversation.add_bot_msg(answer, elapsed_sec)
+
+        for answer in answer_iterator:
+            end = time.time()
+            elapsed_sec = end - start
+            try:
+                self.conversation.append_bot_msg(answer, elapsed_sec)
+            except:
+                # This can happen if the user clicks "Clear" while the AI is
+                # thinking
+                return
+
+        self.progress.visible = False
+        self.progress.update()
+
+        self.tf_input.update()
+        self.tf_input.focus()
+
+    def on_clear(self, vent: ft.ControlEvent) -> None:
+        """This function is called when the user clicks the Clear button. It
+        clears the conversation."""
+        self.conversation.clear()
+        self.conversation.add_bot_msg(INITIAL_MSG)
+        self.progress.visible = False
+        self.progress.update()
+
+    def change_conf(self, new_model_name: str) -> None:
+        """This function is called when the user changes the chat bot. It
+        clears the conversation and displays the initial message."""
+        self.tabs.selected_index = 0
+        self.tabs.update()
+
+        self.progress.visible = True
+        self.progress.update()
+
+        if new_model_name == self.model_name:
+            return
+
+        self.model_name = new_model_name
+
+        self.chat_bot = ChatBot(self.model_name)
+        self.text_log.reset()
+        self.text_log.visible = True
+
+        self.init_model()
+
+        self.conversation.clear()
+        self.conversation.add_bot_msg(INITIAL_MSG)
+
+        self.progress.visible = False
+        self.progress.update()
+
+
+def main(page: ft.Page):
+    """This function is called when the application starts. It creates the
+    flet page and adds the controls to it."""
+    app = ChatBotApp(page)
 
 
 ft.app(target=main, view=ft.WEB_BROWSER, assets_dir="./")
--- configuration.py
+++ configuration.py
@@ -6,10 +6,10 @@ from typing import List
 from huggingface_hub import scan_cache_dir
 import flet as ft
 
-from chat_bot import HH_MODEL_REPOS, MODEL_PARAMS
+from chat_bot import HH_MODEL_REPOS, MODEL_PARAMS, ChatBot
 
 
-def init_config(page: ft.Page) -> str:
+def get_init_config(page: ft.Page) -> str:
     """This function initializes the configuration of the chat bot using flet's
     client storage. It returns the model name."""
     if not page.client_storage.contains_key("model"):
@@ -27,8 +27,11 @@ class ConfigurationControl(ft.UserControl):
         self.page = page
         self.dd_model = None
         self.text_model_info = None
+        self.button_apply = None
+        self.change_bot_hook = None
 
-    def __params_to_str(self, params: dict) -> str:
+    @staticmethod
+    def __params_to_str(params: dict) -> str:
         """This function converts the model parameters to a string."""
         s = ""
         for key, value in params.items():
@@ -40,9 +43,18 @@ class ConfigurationControl(ft.UserControl):
         """This function is called when the user selects a model from the
         dropdown."""
         model_name = self.dd_model.value
-        self.text_model_info.value = self.__params_to_str(MODEL_PARAMS[model_name])
+        self.text_model_info.value = ConfigurationControl.__params_to_str(
+            MODEL_PARAMS[model_name]
+        )
         self.text_model_info.update()
-        self.page.update()
+
+    def __on_apply(self, event):
+        """This function is called when the user clicks the Apply button. It
+        saves the configuration to the client storage."""
+        model_name = self.dd_model.value
+        self.page.client_storage.set("model", model_name)
+
+        self.conf_change_hook(model_name)
 
     def __create_model_name_options(self) -> List[ft.dropdown.Option]:
         """This function creates the options for the model dropdown."""
@@ -55,25 +67,36 @@ class ConfigurationControl(ft.UserControl):
             else:
                 option = f"{HH_MODEL_REPOS[model_name]} (not downloaded)"
             options.append(ft.dropdown.Option(text=option, key=model_name))
-            print(f"key: {model_name}, text: {option}")
 
         return options
 
     def build(self):
-        # model = init_config(self.page)
-        model_name = "DISTILGPT2"
-        print("Default model: " + model_name)
         self.dd_model = ft.Dropdown(
             on_change=self.__dropdown_changed,
             label="Model",
             options=self.__create_model_name_options(),
             autofocus=True,
-            value=HH_MODEL_REPOS[model_name],
         )
 
-        self.text_model_info = ft.Text(self.__params_to_str(MODEL_PARAMS[model_name]))
+        self.text_model_info = ft.Text("")
+
+        self.button_apply = ft.FilledButton(text="Apply", on_click=self.__on_apply)
 
         return ft.Container(
-            content=ft.Column([self.dd_model, self.text_model_info]),
+            content=ft.Column([self.dd_model, self.text_model_info, self.button_apply]),
             padding=ft.Padding(left=0, top=20, right=0, bottom=0),
         )
+
+    def did_mount(self):
+        # This has to be done here because, before, the page is not created
+        model_name = get_init_config(self.page)
+        self.dd_model.value = model_name
+        self.text_model_info.value = ConfigurationControl.__params_to_str(
+            MODEL_PARAMS[model_name]
+        )
+        self.update()
+
+    def set_conf_change_hook(self, hook):
+        """This function sets the hook that is called when the user clicks the
+        Apply button."""
+        self.conf_change_hook = hook
